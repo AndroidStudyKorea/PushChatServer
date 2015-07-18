0ac08f2b41480b8fbaf3c1aa81ae2e71cab75597
Sunho, Bang <snoya00@gmail.com>
2015-07-18 18:25:23 +0900

    Add Talk model

--- app/serializers.py
+++ app/serializers.py
@@ -1,21 +1,8 @@
 from rest_framework import serializers
-from app.models import Snippet
-from django.contrib.auth.models import User
+from app.models import Talk
 
 
-class SnippetSerializer(serializers.HyperlinkedModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
-    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+class TalkSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Snippet
-        fields = ('url', 'highlight', 'owner',
-                  'title', 'code', 'linenos', 'language', 'style')
-
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    snippets = serializers.HyperlinkedRelatedField(queryset=Snippet.objects.all(), view_name='snippet-detail', many=True)
-
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'snippets')
+        model = Talk
--- proj/urls.py
+++ proj/urls.py
@@ -1,28 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-import rest_framework_nested
-from rest_framework_nested.routers import SimpleRouter
-
+from app.views import TalkViewSet
 from proj.routers import DefaultRouter
-from app.views import SnippetViewSet
-# from app.views import UserViewSet
-from notice.views import UserViewSet
-from notice.views import ArticleViewSet
-from notice.views import CommentViewSet
+
 
 admin.autodiscover()
 
 router = DefaultRouter()
-router.register(r'snippets', SnippetViewSet)
-router.register(r'users', UserViewSet)
-router.register(r'notice', ArticleViewSet, base_name='donations')
-
-nested_router = rest_framework_nested.routers.NestedSimpleRouter(router, r'notice', lookup='donations')
-nested_router.register(r'comment', CommentViewSet)
+router.register(r'talk', TalkViewSet)
 
 urlpatterns = patterns('',
-                       url(r'^api/v1/', include(router.urls)),
-                       url(r'^api/v1/', include(nested_router.urls)),
-                       url(r'^api/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^api/', include(router.urls)),
                        )
